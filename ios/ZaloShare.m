9cc85521d9c192d86d5458d12414f976c08adfa5
Bui Thanh Ba Vuong <bavuongco10@gmail.com>
2018-05-28 10:42:20 +0700

    .

--- ios/ZaloShare.m
+++ ios/ZaloShare.m
@@ -18,13 +18,8 @@
         
         NSURL *zaloURL = [NSURL URLWithString:[url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
         
-        if ([[UIApplication sharedApplication] canOpenURL: zaloURL]) {
-            [[UIApplication sharedApplication] openURL:zaloURL];
-            successCallback(@[]);
-        } else {
-            // Cannot open gplus
-            NSLog(@"error web intent");
-        }
+        [[UIApplication sharedApplication] openURL:zaloURL];
+        successCallback(@[]);
     }
 }
 
